0c48baa2d9939a946e8bd965c4b2778ec7d81d4e
Shreya Chandra <shreyachandra@Shreyas-MacBook-Pro.local>
2020-10-21 19:27:40 +0530

    comment fix

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     			// Implement functionality: On button click, a new element should appear with the text
 				// to "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy".
 				// This text is supposed to be in p tags with id="para"
-				// This element remains as is after the first click. (No change on further clicking). */
+				// This element remains as is after the first click. (No change on further clicking).
 				}
     		</div>
     		);
