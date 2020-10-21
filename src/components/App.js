0daa0463e5867e815eaf09d2da077cdb64d77d3c
Shreya Chandra <shreyachandra@Shreyas-MacBook-Pro.local>
2020-10-21 19:38:48 +0530

    remove hints

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,16 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props)
-	{
+    constructor(props) {
 		super(props);
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ 
-				// Do not remove this main div!!
-    			// Make a button with id="click"
-    			// Implement functionality: On button click, a new element should appear with the text
-				// to "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy".
-				// This text is supposed to be in p tags with id="para"
-				// This element remains as is after the first click. (No change on further clicking).
-				}
+				{ /* Do not remove this main div!! */ }
     		</div>
-    		);
+    	);
     }
 }
 
